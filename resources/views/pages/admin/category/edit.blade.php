ffb0db4ba8c705bb83a2ffa017f6f76246a9a950
Aes Saputra <aessaputra@yahoo.com>
2025-03-25 16:10:34 +0700

    feat(admin/report-category): update data

--- resources/views/pages/admin/category/edit.blade.php
+++ resources/views/pages/admin/category/edit.blade.php
@@ -1,59 +1,45 @@
 @extends('layouts.admin')
 
-@section('title', 'Edit Data Masyarakat')
+@section('title', 'Edit Data Kategori')
 
 @section('content')
-    <a href="{{ route('admin.resident.index') }}" class="btn btn-danger mb-3">Kembali</a>
+    <a href="{{ route('admin.report-category.index') }}" class="btn btn-danger mb-3">Kembali</a>
 
     <div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Edit Data</h6>
-    </div>
-    <div class="card-body">
-        <form action="{{ route('admin.resident.update', $resident->id) }}" method="POST" enctype="multipart/form-data">
-          @csrf
-          @method('PUT')
-            <div class="form-group">
-                <label for="name">Nama</label>
-                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name', $resident->user->name) }}">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Edit Data</h6>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('admin.report-category.update', $category->id) }}" method="POST"
+                enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="name">Nama</label>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                        name="name" value="{{ old('name', $category->name) }}">
 
-                @error('name')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div>
-                @enderror
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email', $resident->user->email) }}" readonly>
-
-                @error('email')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div>
-                @enderror
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
-
-              @error('password')
-                <div class="invalid-feedback">
-                  {{ $message }}
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-              @enderror
-            </div>
-            <div class="form-group">
-              <label for="avatar">Foto Profile</label>
-              <input type="file" class="form-control @error('avatar') is-invalid @enderror" id="avatar" name="avatar">
+                <div class="form-group">
+                    <p>Gambar / Ikon Lama</p>
+                    <img src="{{ asset('storage/' . $category->image) }}" alt="image" width="200">
+                    <br>
+                    <label for="image">Gambar / Ikon</label>
+                    <input type="file" class="form-control @error('image') is-invalid @enderror" id="image"
+                        name="image">
 
-              @error('avatar')
-                <div class="invalid-feedback">
-                  {{ $message }}
+                    @error('image')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-              @enderror
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-@endsection
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    @endsection
