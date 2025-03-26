fe5a1f776bd2a75275e3d074fb9bacef55b6008e
aessaputra <aessaputra@yahoo.com>
2025-03-26 14:26:49 +0000

    feat(frontend): show category report

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @yield('content')
     </div>
 
-    @include('includes.nav-movile')
+    @include('includes.nav-mobile')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
