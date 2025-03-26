dfc6c5288ccf44b357bf8fcc17f69174fe204b3b
aessaputra <aessaputra@yahoo.com>
2025-03-26 16:42:20 +0000

    feat(frontend): detail laporan

--- app/Repositories/ReportRepository.php
+++ app/Repositories/ReportRepository.php
@@ -17,6 +17,11 @@ class ReportRepository implements ReportRepositoryInterface
     return Report::latest()->get()->take(5);
   }
 
+  public function getReportByCode(string $code)
+  {
+    return Report::where('code', $code)->first();
+  }
+
   public function getReportById(int $id)
   {
     return Report::where('id', $id)->first();
